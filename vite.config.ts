2d8bb6650a771f9375e893567fd8499da07e22bb
Novaesweb <264890180+Novaesweb@users.noreply.github.com>
2026-03-20 02:41:52 -0300

    fix: increase workbox maximumFileSizeToCacheInBytes to allow build with larger assets

--- vite.config.ts
+++ vite.config.ts
@@ -41,6 +41,7 @@ export default defineConfig(({ mode }) => ({
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
         navigateFallbackDenylist: [/^\/~oauth/],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MiB
       },
     }),
   ].filter(Boolean),
