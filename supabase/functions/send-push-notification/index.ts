45a169a045c0510f912dfa52574473080be1d16f
Novaesweb <264890180+Novaesweb@users.noreply.github.com>
2026-03-23 12:01:11 -0300

    fix: ignore npm import lint error in push notification function

--- supabase/functions/send-push-notification/index.ts
+++ supabase/functions/send-push-notification/index.ts
@@ -2,6 +2,7 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 // @ts-ignore
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+// @ts-ignore
 import webPush from "npm:web-push@3";
 
 declare const Deno: any;
