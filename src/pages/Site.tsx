67347e01b9a36013b063278aa0660b0ac1cc1fa8
Novaesweb <264890180+Novaesweb@users.noreply.github.com>
2026-03-25 03:11:41 -0300

    refactor(site): move Authority Seal to footer only

--- src/pages/Site.tsx
+++ src/pages/Site.tsx
@@ -12,7 +12,6 @@ import ProcessoSection from "@/components/site/ProcessoSection";
 import PlanosSection from "@/components/site/PlanosSection";
 import FuncionalidadeExtraSection from "@/components/site/FuncionalidadeExtraSection";
 import ResultadosSection from "@/components/site/ResultadosSection";
-import InfrastructureBadgeSection from "@/components/site/InfrastructureBadgeSection";
 import SiteModals from "@/components/site/SiteModals";
 import CadastroPerfeitoSection from "@/components/site/CadastroPerfeitoSection";
 import SiteFooter from "@/components/site/SiteFooter";
@@ -40,7 +39,6 @@ export default function Site() {
       <PlanosSection />
       <FuncionalidadeExtraSection />
       <ResultadosSection />
-      <InfrastructureBadgeSection />
       <CadastroPerfeitoSection />
       <SiteModals modalOpen={modalOpen} onClose={() => setModalOpen(null)} />
       <SiteFooter onOpenModal={setModalOpen} />
